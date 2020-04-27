da0b7f75c5657bf3793216e33b43cec56f49b489
v-bui <v-bui@dimage.co.jp>
2020-04-27 10:10:04 +0700

    insert log to databases;

--- src/main/java/log4j/Demo.java
+++ src/main/java/log4j/Demo.java
@@ -3,14 +3,10 @@ package log4j;
 import org.apache.log4j.Logger;
 
 public class Demo {
-	private static final Logger logger = Logger.getLogger(Demo.class);
-
-	public static void main(String[] args) {
-		for (int i = 0; i < 1000; i++) {
-			logger.debug("bedug log");
-			logger.error("error log");
-			logger.info("info log");
-		}
-	}
-
+  private static final Logger logger = Logger.getLogger(Demo.class);
+  public static void main(String[] args) {
+    logger.debug("debug log");
+    logger.error("error log");
+    logger.info("info log");
+  }
 }
